e3899de114a51af8e3df1ada1bfca76890280421
Prasad <sp.dobest@gmail.com>
2018-03-23 22:20:56 +0530

    Initial Commit

---
+++ MyNews App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyNews App
+//
+//  Created by sibaprasad on 23/03/18.
+//  Copyright © 2018 SP inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
