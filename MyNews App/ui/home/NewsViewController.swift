cb12f3d7b85785d329fcebd42fb819585979bdfa
spdobest <sp.dobest@gmail.com>
2018-03-28 14:17:49 +0530

    Image Loader Added
    asda

--- MyNews App/ui/home/NewsViewController.swift
+++ MyNews App/ui/home/NewsViewController.swift
@@ -179,6 +179,15 @@ extension NewsViewController : UITableViewDelegate {
     }
     
     
+    let url = URL(string: image.url)
+
+DispatchQueue.global().async {
+    let data = try? Data(contentsOf: url!) //make sure your image in this url does exist, otherwise unwrap in a if let check / try-catch
+    DispatchQueue.main.async {
+        imageView.image = UIImage(data: data!)
+    }
+}
+    
     
 }
 
